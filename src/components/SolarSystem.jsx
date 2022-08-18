c75a6a6fcb91ef63dc8b5b3defbe9058071448e0
Cristiano <weydsoncristiano@hotmail.com>
2022-08-18 18:36:38 -0300

    resolução6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {
+          planets.map((planeta) => (
+            <PlanetCard
+              key={ planeta.name }
+              planetName={ planeta.name }
+              planetImage={ planeta.image }
+            />
+          ))
+        }
       </div>
     );
   }
