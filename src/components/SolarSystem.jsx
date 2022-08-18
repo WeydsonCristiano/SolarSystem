883e2a9eaa28dff1f80ebdb7679581278101c023
Cristiano <weydsoncristiano@hotmail.com>
2022-08-18 14:58:58 -0300

    requisito 2

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+class SolarSystem extends React.Component {
+    render() {
+        return <div data-testid='solar-system'></div>;
+        }
+}
+
+export default App;
