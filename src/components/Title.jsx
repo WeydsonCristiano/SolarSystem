4e9da3ec90301e65c8211b4da51181670f980e55
Cristiano <weydsoncristiano@hotmail.com>
2022-08-18 17:49:40 -0300

    correção 3

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,16 @@
-import react from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
-    return;
-    <h2 />;
+    const { headline } = this.props;
+    return (
+      <h2>{headline}</h2>
+    );
   }
 }
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
 
-export default App;
+export default Title;
