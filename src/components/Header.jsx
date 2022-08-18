cbccd0049d019168b2548ce8646aeca6579b8c84
Cristiano <weydsoncristiano@hotmail.com>
2022-08-18 14:52:44 -0300

    requisito 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+class header extends React.Component {
+  render() {
+    return(
+    <Header>
+      <H1>Sistema Solar</H1>;
+    </Header>
+  )}
+}
+
+export default App;
