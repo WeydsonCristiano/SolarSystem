9af52facff2c91e9582ba6aebbbadcfb8d826a38
Cristiano <weydsoncristiano@hotmail.com>
2022-08-18 15:03:25 -0300

    andamento do projeto3

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+class Title extends React.Component{
+    render(){
+        return
+        <h2>
+            <headline></headline>
+        </h2>
+    }
+}
+
+export default App;
