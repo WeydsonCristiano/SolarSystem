eb4b732d36d0d4747ba7a102579392e1b8822577
Cristiano <weydsoncristiano@hotmail.com>
2022-08-19 12:05:13 -0300

    iniciando css

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -7,7 +7,11 @@ class PlanetCard extends React.Component {
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <img
+          className="PlanetaCard"
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+        />
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -6,7 +6,7 @@ import planets from '../data/planets';
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <div className="Planetas" data-testid="solar-system">
         <Title headline="Planetas" />
         {
           planets.map((planeta) => (
